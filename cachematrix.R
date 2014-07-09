7f747325538e832314bb6771e86cb70195fdee96
samarthbhargav <samarth.bhargav92@gmail.com>
2014-07-09 23:18:12 +0530

    Added Code, eh?

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve are methods that allows caching of a matrix inverse
+## Usage: makeCacheMatrix([existing matrix]) returns a CachableMatrix
+## Instead of calling solve(matrix), call cacheSolve(matrix), and you're done!
 
-## Write a short comment describing this function
+## Returns a Matrix that allows computation of its inverse and caches it
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+    # inverse is NULL when we start off
+    inverse <- NULL
+    
+    # function that sets the matrix, and sets inverse to NULL
+    set <- function(y) {
+      mat <<- y
+      inverse <<- NULL
+    }
+    # function that gets the matrix
+    get <- function() {
+      return(mat)
+    }
+    # function that sets the inverse of the matrix
+    setinverse <- function(inv) {
+      inverse <<- inv
+    }
+    # function that gets the inverse of the matrix
+    getinverse <- function() {
+      # in my opinion, cacheSolve should be called here. More OOP-y
+      return(inverse)
+    }
+    
+    # construct the list object and return it  
+    return(list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse))
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat, ...) {
+  
+    # first, check if inverse has been computed, if no, compute
+    if(is.null(mat$getinverse())) { # inverse has not been computed
+      
+      # get the inverse
+      inverse <- solve(mat$get()) 
+      # cache it
+      mat$setinverse(inverse)
+    }
+    
+    return(mat$getinverse())
 }
+
+
+##### Tests ##### 
+# uncomment to test
+
+# create a 10x10 matrix
+mat <- makeCacheMatrix(matrix(rnorm(100), c(10,10)))
+stopifnot(!is.null(mat$get()))
+print("get() Works")
+mat$set(matrix(rnorm(10000), c(100,100)))
+stopifnot(dim(mat$get()) == c(100,100))
+print("set() works")
+stopifnot(is.null(mat$getinverse()))
+print("inverse is correctly null")
+# calculating and caching inverse
+cacheSolve(mat)
+stopifnot(!is.null(mat$getinverse()))
+print("cachesolve seems to work")
+print("If the next print occurs immediately, then caching is working")
+for(i in 1:10000) {
+  cacheSolve(mat)
+}
+stopifnot(!is.null(mat$getinverse()))
+print("Called cacheSolve() 10,000 times")
