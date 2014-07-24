398c6e0b317d615a83fdc5d0f185cc191dee83c8
samarthbhargav <samarth.bhargav92@gmail.com>
2014-07-24 12:14:51 +0530

    Commented out test for submission

--- cachematrix.R
+++ cachematrix.R
@@ -43,6 +43,7 @@ cacheSolve <- function(mat, ...) {
 
       # get the inverse
       inverse <- solve(mat$get()) 
+      
       # cache it
       mat$setinverse(inverse)
     } else {
@@ -57,21 +58,21 @@ cacheSolve <- function(mat, ...) {
 # comment out the message("getting cached data") before running this test
 
 # create a 10x10 matrix
-mat <- makeCacheMatrix(matrix(rnorm(100), c(10,10)))
-stopifnot(!is.null(mat$get()))
-print("get() Works")
-mat$set(matrix(rnorm(10000), c(100,100)))
-stopifnot(dim(mat$get()) == c(100,100))
-print("set() works")
-stopifnot(is.null(mat$getinverse()))
-print("inverse is correctly null")
-# calculating and caching inverse
-cacheSolve(mat)
-stopifnot(!is.null(mat$getinverse()))
-print("cachesolve seems to work")
-print("If the next print occurs immediately, then caching is working")
-for(i in 1:10000) {
-  cacheSolve(mat)
-}
-stopifnot(!is.null(mat$getinverse()))
-print("Called cacheSolve() 10,000 times")
+#mat <- makeCacheMatrix(matrix(rnorm(100), c(10,10)))
+#stopifnot(!is.null(mat$get()))
+#print("get() Works")
+#mat$set(matrix(rnorm(10000), c(100,100)))
+#stopifnot(dim(mat$get()) == c(100,100))
+#print("set() works")
+#stopifnot(is.null(mat$getinverse()))
+#print("inverse is correctly null")
+#calculating and caching inverse
+#cacheSolve(mat)
+#stopifnot(!is.null(mat$getinverse()))
+#print("cachesolve seems to work")
+#print("If the next print occurs immediately, then caching is working")
+#for(i in 1:10000) {
+#  cacheSolve(mat)
+#}
+#stopifnot(!is.null(mat$getinverse()))
+#print("Called cacheSolve() 10,000 times")
